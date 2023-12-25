57dc7bfe1bc6a8b0d047be3b6a3c09f0986fd697
Keertan Kuppili <90203723+KeEbEe123@users.noreply.github.com>
2023-12-25 18:36:02 +0530

    Add files via upload

---
+++ weather app/script.js
@@ -0,0 +1,36 @@
+let url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Hyderabad`;
+
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '2431ac6813mshdfd7013cc52e529p1e9289jsnc3af44fa95af',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+}
+const getWeather = (city)=>
+{
+    cityName.innerHTML=city;
+    fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`,options)
+    .then(response => response.json())
+    .then((response)=>{console.log(response);    
+        if(response.temp>=20){
+        $('.weather-icon').html('<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="current-color" class="bi bi-brightness-alt-high" viewBox="0 0 16 16"><path d="M8 3a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 3m8 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5m-13.5.5a.5.5 0 0 0 0-1h-2a.5.5 0 0 0 0 1zm11.157-6.157a.5.5 0 0 1 0 .707l-1.414 1.414a.5.5 0 1 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m-9.9 2.121a.5.5 0 0 0 .707-.707L3.05 5.343a.5.5 0 1 0-.707.707l1.414 1.414zM8 7a4 4 0 0 0-4 4 .5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5 4 4 0 0 0-4-4m0 1a3 3 0 0 1 2.959 2.5H5.04A3 3 0 0 1 8 8"/></svg>');}
+        else{
+            $('.weather-icon').html('<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="current-color" class="bi bi-snow" viewBox="0 0 16 16"><path d="M8 16a.5.5 0 0 1-.5-.5v-1.293l-.646.647a.5.5 0 0 1-.707-.708L7.5 12.793V8.866l-3.4 1.963-.496 1.85a.5.5 0 1 1-.966-.26l.237-.882-1.12.646a.5.5 0 0 1-.5-.866l1.12-.646-.884-.237a.5.5 0 1 1 .26-.966l1.848.495L7 8 3.6 6.037l-1.85.495a.5.5 0 0 1-.258-.966l.883-.237-1.12-.646a.5.5 0 1 1 .5-.866l1.12.646-.237-.883a.5.5 0 1 1 .966-.258l.495 1.849L7.5 7.134V3.207L6.147 1.854a.5.5 0 1 1 .707-.708l.646.647V.5a.5.5 0 1 1 1 0v1.293l.647-.647a.5.5 0 1 1 .707.708L8.5 3.207v3.927l3.4-1.963.496-1.85a.5.5 0 1 1 .966.26l-.236.882 1.12-.646a.5.5 0 0 1 .5.866l-1.12.646.883.237a.5.5 0 1 1-.26.966l-1.848-.495L9 8l3.4 1.963 1.849-.495a.5.5 0 0 1 .259.966l-.883.237 1.12.646a.5.5 0 0 1-.5.866l-1.12-.646.236.883a.5.5 0 1 1-.966.258l-.495-1.849-3.4-1.963v3.927l1.353 1.353a.5.5 0 0 1-.707.708l-.647-.647V15.5a.5.5 0 0 1-.5.5z"/></svg>')
+        }
+        temp.innerHTML = `${response.temp}`;
+        wind.innerHTML = `${response.wind_speed}`;
+        humidity.innerHTML = `${response.humidity}`;
+        
+    }).catch (console.error());
+
+
+    
+}
+
+submit.addEventListener("click",(e)=>{
+    getWeather(city.value);
+    console.log(city.value)
+    e.preventDefault();
+})
+
